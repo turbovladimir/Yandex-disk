68664990b7a0bf83817c6423edd39c971daa9c1b
v.sadovnikov <v.sadovnikov@cityads.team>
2020-01-21 12:38:07 +0300

    delete config.php

--- public_html/index.php
+++ public_html/index.php
@@ -6,6 +6,5 @@
  * Time: 14:23
  */
 require dirname(__DIR__).'/vendor/autoload.php';
-require dirname(__DIR__). '/config.php';
 $controllerFront = new \App\YandexDisk\ControllerFront();
 $controllerFront->index($_GET, $_POST, $_COOKIE);
